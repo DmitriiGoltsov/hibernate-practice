171f934d530b7353e9e28b68ec3b143e295dedbe
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2024-03-31 21:47:32 +0300

    feat: add running code to demonstrate how entities work in Hibernate

--- src/main/java/org/goltsov/HibernateRunner.java
+++ src/main/java/org/goltsov/HibernateRunner.java
@@ -1,19 +1,39 @@
 package org.goltsov;
 
+import entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
 
 public class HibernateRunner {
 
     public static void main(String[] args) {
         Configuration configuration = new Configuration();
         configuration.configure();
+        configuration.addAnnotatedClass(User.class);
 
         try (SessionFactory factory = configuration.buildSessionFactory();
             Session session = factory.openSession()) {
 
-            System.out.println("Ok. Session is " + session.getClass().getCanonicalName());
+            Transaction transaction = session.beginTransaction();
+
+            User user = User.builder()
+                    .username("PussyDestroyer1811@gmail.com")
+                    .name("Gaylord")
+                    .surname("Fucker")
+                    .birthday(LocalDate.of(1999, Month.DECEMBER, 15))
+                    .age(25)
+                    .build();
+
+            session.persist(user);
+
+            transaction.commit();
+
+
         }
     }
 }
