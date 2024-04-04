9300393d9e542c92dde172b4d3c18ad46dc229f4
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2024-04-04 21:08:07 +0300

    feat: add a new class for a field and its convertor

---
+++ src/main/java/converter/BirthdayConvertor.java
@@ -0,0 +1,26 @@
+package converter;
+
+import entity.Birthday;
+import jakarta.persistence.AttributeConverter;
+
+import java.sql.Date;
+import java.util.Optional;
+
+public class BirthdayConvertor implements AttributeConverter<Birthday, Date> {
+
+    @Override
+    public Date convertToDatabaseColumn(Birthday birthday) {
+        return Optional.ofNullable(birthday)
+                .map(Birthday::birthday)
+                .map(Date::valueOf)
+                .orElse(null);
+    }
+
+    @Override
+    public Birthday convertToEntityAttribute(Date date) {
+        return Optional.ofNullable(date)
+                .map(Date::toLocalDate)
+                .map(Birthday::new)
+                .orElse(null);
+    }
+}
